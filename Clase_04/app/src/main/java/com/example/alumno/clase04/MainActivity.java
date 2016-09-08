4926b885a255e709792ff6741f63bf658603ea15
unknown <alumno@LAB02PC15.labs.extension>
2016-09-08 20:28:22 -0300

    modificacion clase 04

--- Clase_04/app/src/main/java/com/example/alumno/clase04/MainActivity.java
+++ Clase_04/app/src/main/java/com/example/alumno/clase04/MainActivity.java
@@ -20,8 +20,13 @@ public class MainActivity extends AppCompatActivity {
         personas.add((new Persona ("Nombre1","Apellido1")));
         personas.add((new Persona ("Nombre2","Apellido2")));
         personas.add((new Persona ("Nombre3","Apellido3")));
-        personas.add((new Persona ("Nombre4","Apellido4")));
-        personas.add((new Persona ("Nombre5","Apellido5")));
+        personas.add((new Persona("Nombre4", "Apellido4")));
+        personas.add((new Persona("Nombre5", "Apellido5")));
+        personas.add((new Persona ("Nombre6","Apellido6")));
+        personas.add((new Persona ("Nombre7","Apellido7")));
+        personas.add((new Persona ("Nombre8","Apellido8")));
+        personas.add((new Persona ("Nombre9","Apellido9")));
+        personas.add((new Persona("Nombre10", "Apellido10")));
 
         RecyclerView rv = (RecyclerView) findViewById(R.id.rvPersona);
         MiAdapter miAdapter = new MiAdapter(personas);
